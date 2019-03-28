1b71b77ce2b8ee1d6cd51d1ebfe307fda1eb8b8c
Zeljko Grujic <zgrujic@carnegietechnologies.com>
2019-03-28 15:18:02 +0100

    Comments fixed

--- app/src/androidTest/java/com/example/tijana/myapplication/AppFunctionalityTest.kt
+++ app/src/androidTest/java/com/example/tijana/myapplication/AppFunctionalityTest.kt
@@ -5,22 +5,12 @@ import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.runner.AndroidJUnit4
-
-
 import org.junit.Test
 import org.junit.runner.RunWith
-
-
 import org.junit.Rule
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class AppFunctionalityTest {
-
     val CORRECT_LOGIN_TEXT = "LOGIN"
 
     @Rule
@@ -29,11 +19,8 @@ class AppFunctionalityTest {
 
     @Test
     fun testingLoginButton() {
-        // Context of the app under test.
-        onView(withId(R.id.login_button)).check(matches(withText(CORRECT_LOGIN_TEXT)))
         onView(withId(R.id.login_button)).check(matches(isDisplayed()))
+        onView(withId(R.id.login_button)).check(matches(withText(CORRECT_LOGIN_TEXT)))
         onView(withId(R.id.login_button)).check(matches(isClickable()))
     }
-
-
 }
