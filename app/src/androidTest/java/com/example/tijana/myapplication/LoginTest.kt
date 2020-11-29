263379508d9f586fadbbc77e69dc869c22db76a3
Tijana Stikic <tstikic@carnegietechnologies.com>
2020-11-29 16:17:34 +0100

    Redesigning project

---
+++ app/src/androidTest/java/com/example/tijana/myapplication/LoginTest.kt
@@ -0,0 +1,25 @@
+package com.example.tijana.myapplication
+
+import android.support.test.espresso.Espresso.closeSoftKeyboard
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.typeText
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LoginTest {
+
+    @Rule
+    @JvmField
+    val activityTestRule = ActivityTestRule(MainActivity::class.java, false, true)
+
+    @Test
+    fun exampleTest() {
+    }
+}
